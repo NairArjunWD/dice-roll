1d5baf6454fc6d53d1bbde639e6167d7cb37d67f
Arjun Nair <nair.arjun47@yahoo.com>
2020-01-15 10:55:00 -0800

    Showing two dice now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component} from 'react';
-import Die from './Die';
+import RollDice from './RollDice';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Die face='one' />
-        <Die face='two' />
-        <Die face='three' />
-        <Die face='four' />
-        <Die face='five' />
-        <Die face='six' />
+        <RollDice />
       </div>
     );
   }
