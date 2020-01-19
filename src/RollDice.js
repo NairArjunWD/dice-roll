fb28a087084c8c47aef284385738a0d26842e516
Arjun Nair <nair.arjun47@yahoo.com>
2020-01-18 23:47:10 -0800

    Roll button says rolling when pressed

--- src/RollDice.js
+++ src/RollDice.js
@@ -7,7 +7,7 @@ class RollDice extends Component {
     };
     constructor(props){
         super(props);
-        this.state = {die1: 'one', die2: 'one'}
+        this.state = { die1: 'one', die2: 'one', rolling: false }
         this.roll = this.roll.bind(this);
     }
     roll(){
@@ -15,7 +15,7 @@ class RollDice extends Component {
         const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
         const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
         // set state with new rolls
-        this.setState({die1: newDie1, die2: newDie2});
+        this.setState({ die1: newDie1, die2: newDie2, rolling: true });
     }
     render() {
         return (
@@ -24,7 +24,9 @@ class RollDice extends Component {
                     <Die face={this.state.die1}/>
                     <Die face={this.state.die2}/>
                 </div>
-                <button onClick={this.roll}>Roll</button>
+                <button onClick={this.roll}>
+                    {this.state.rolling ? 'Rolling...' : 'Roll Dice'}
+                </button>
             </div>
         )
     }
