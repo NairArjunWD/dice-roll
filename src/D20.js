00c93396107ce27d930727adc512687bb91a1931
Arjun Nair <nair.arjun47@yahoo.com>
2020-02-20 13:03:24 -0800

    Create D20 files

---
+++ src/D20.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './D20.css'
+
+class D20 extends Component {
+    render() {
+        return (
+            <div>
+                <i class="fas fa-dice-d20"></i>
+            </div>
+        )
+    }
+}
+
+export default D20;
