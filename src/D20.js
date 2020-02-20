08701acefc2ca935577e6798068e403ca4fdd1b8
Arjun Nair <nair.arjun47@yahoo.com>
2020-02-20 13:21:41 -0800

    Test char appears on D20 die

--- src/D20.js
+++ src/D20.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import './D20.css'
 
 class D20 extends Component {
+
     render() {
         return (
             <div>
-                <i class="fas fa-dice-d20"></i>
+                <i class="fas fa-dice-d20">
+                    <print class='num'>
+                        z
+                    </print>
+                </i>
             </div>
         )
     }
